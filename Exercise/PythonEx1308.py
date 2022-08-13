824b7d16152bada6a6e176f88947be676ab1135c
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-08-14 00:23:01 +0200

    exercises Game finished

--- Exercise/PythonEx1308.py
+++ Exercise/PythonEx1308.py
@@ -6,8 +6,8 @@ def main():
     #print(intsIn("10 and 3 are like alot of 200"))
     #fizzBuzz()
     #print(addNumbers([5,6,10], [5,8,19], [10, 12, 3, 18]))
-    monkeyAuthor()
-
+    #monkeyAuthor()
+    mastermind()
 
 
 def starTriangle(n):
@@ -78,7 +78,40 @@ def monkeyAuthor():
         print(" ".join(sentence2))
 
 
+def mastermind():
+    num = random.randrange(999, 9999)
+    counter = 10
+    while counter > 0:
+        output = []
+        counter-=1
+        try:
+            numIn = int(input("Four digit number!"))
+        except ValueError:
+            print("A >>>>>>>>NUMBER<<<<<<<<<")
+            continue
+        if len(str(numIn)) != 4:
+            print("Four digits.....four")
+            continue
 
+        else:
+            numInL = list(str(numIn))
+            numL = list(str(num))
+
+            for x in numInL:
+                if x in numL:
+                    if numL.index(x) == numInL.index(x):
+                        output.append("R")
+                    else:
+                        output.append("Y")
+
+                else:
+                    output.append("B")
+        if numIn == num:
+            print(f"you done did it, and it took {10-counter} tries!")
+            break
+        else:
+            print(f'Result: {"".join(output)} yout have {counter} left')
+            
 
 
 if __name__ == "__main__":
