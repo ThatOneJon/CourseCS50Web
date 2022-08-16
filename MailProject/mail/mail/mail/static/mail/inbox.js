2d12d8a65878b526be7db351b2538d288ab00eb0
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-08-16 11:05:56 +0200

    next

--- MailProject/mail/mail/mail/static/mail/inbox.js
+++ MailProject/mail/mail/mail/static/mail/inbox.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   // By default, load the inbox
-  load_mailbox('inbox');
+    load_mailbox('inbox');
 });
 
 function compose_email() {
@@ -20,7 +20,24 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.addEventListener("submit", () => {
+  fetch('/emails',{
+    method: 'POST',
+    body: JSON.stringify({
+      recipients : "jon@doe.com",
+      subject : document.getElementById("compose-subject").value,
+      body : document.getElementById("compose-body").value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+}) });
 }
+
+
 
 function load_mailbox(mailbox) {
   
@@ -28,6 +45,25 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails =>{
+
+    console.log(emails);
+
+    emails.forEach( obj=> {
+    const u = document.getElementById("emails-view");
+    let i = document.createElement("li");
+    i.innerHTML = `ID: ${obj.id} - Sender: ${obj.sender} - Subject: ${obj.subject} - FROM: ${obj.timestamp}`;
+    u.appendChild(i);
+    })
+  });
+
+
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+
+  
 }
